926e5fd65c570aa6ff9612aae9246575917707af
Golovanya <golovanya1507@gmail.com>
2024-08-03 14:02:35 +0300

    ready to docker

---
+++ Backend/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import fs from "fs";
+import path from 'path';
+import csv from 'csv-parser'
+
+
+const app = express()
+
+
+
+const port = 8080
+app.use(cors());
+  let results = []
+  fs.createReadStream('data.csv')
+  .pipe(csv())
+  .on('data', (data) => results.push(data))
+  .on('end', () => {
+    console.log(results);
+ 
+  });
+  app.get('/data', (req, res) => {
+    res.send(results);
+  });
+  app.listen(port, () => {
+    console.log(`Сервер запущен на порту ${port}`)
+  })
---
+++ Frontend/src/App.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { AgGridReact } from "ag-grid-react"; // React Data Grid Component
+import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the Data Grid
+import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the Data Grid
+
+async function fetchData() {
+  try {
+    const { data } = await axios.get("http://localhost:8080/data");
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return {};
+  }
+}
+
+function App() {
+  const [data, setData] = useState(null);
+  const fetchDataAsync = async () => {
+    const fetchedData = await fetchData();
+    setData(fetchedData);
+  };
+  useEffect(() => {
+    fetchDataAsync();
+  }, []);
+
+  console.log(data ? typeof data : "net");
+  const [colDefs] = useState([
+    { field: "articleid", flex: 1 },
+    { field: "subarticleid", flex: 1 },
+    { field: "articlename", flex: 1 },
+    { field: "external_str_id", flex: 1 },
+    { field: "ecrlongname", flex: 2 },
+    { field: "sectionname", flex: 1 },
+  ]);
+
+  return (
+    <div className="container">
+      <div
+        className="ag-theme-quartz-dark" // applying the Data Grid theme
+        style={{ height: 500 }} // the Data Grid will fill the size of the parent container
+      >
+        <AgGridReact
+          rowData={data}
+          columnDefs={colDefs}
+          pagination={true}
+          paginationPageSize={50}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
